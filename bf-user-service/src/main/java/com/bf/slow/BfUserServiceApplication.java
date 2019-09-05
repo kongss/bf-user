44aca2b57b16a5f26e5e37322d04563f4b806dc0
kongshuaishuai_Pro <629074622@qq.com>
2019-09-05 15:58:55 +0800

    bf

--- bf-user-service/src/main/java/com/bf/slow/BfUserServiceApplication.java
+++ bf-user-service/src/main/java/com/bf/slow/BfUserServiceApplication.java
@@ -1,32 +1,17 @@
 package com.bf.slow;
 
-import com.bf.slow.api.impl.UserInfoImpl;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.bf.slow.dao")
 @EnableDubbo(scanBasePackages = "com.bf.slow.api")
 public class BfUserServiceApplication {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
     public static void main(String[] args) {
         SpringApplication.run(BfUserServiceApplication.class, args);
     }
 
-    @Autowired
-    UserInfoImpl userInfo;
-
-    @Bean
-    public ApplicationRunner runner(){
-        return args -> logger.info("S: "+ userInfo.getById("1"));
-    }
 }
